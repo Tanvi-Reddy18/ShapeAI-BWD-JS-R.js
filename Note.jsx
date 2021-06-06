7189b4c5163a885d4521231e3a101b8a7f270c57
Tanvi-Reddy18 <85431102+Tanvi-Reddy18@users.noreply.github.com>
2021-06-06 18:52:03 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <p>
+        This is a Basic Web Development with Javascript and React.js conducted
+        by ShapeAI
+      </p>
+      <p> They taught us JavaScript,HTML,React.js at the basic level </p>
+      <p>
+        This is the project that we need to submit in order to get the
+        certificate from ShapeAI
+      </p>
+      <p> This bootcamp was fun and taught me a lot. Thank you! </p>
+    </div>
+  );
+}
+export default Note;
